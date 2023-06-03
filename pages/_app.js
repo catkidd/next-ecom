73b9d96de26d6c655a4755dfdaeee284fe0296ea
catkidd <catkidd770@gmail.com>
2023-06-04 00:39:06 +0545

    init: initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Header />
             <Component {...pageProps} />
+            <Footer />
         </>
     );
 }
